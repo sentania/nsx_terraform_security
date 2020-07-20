3dc110998343f5dd9c8063b817580d2a204ce857
rpletka <rpletka@vmware.com>
2020-07-20 18:13:43 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,18 @@ resource "nsxt_ip_set" "ProtectedAssets" {
   display_name = "Protected Assets"
   ip_addresses = ["10.100.65.0/24"]
 }
+
+resource "nsxt_ip_set" "Internet" {
+  description  = "Internet IPs provisioned by Terraform"
+  display_name = "Internet IPs"
+  ip_addresses = ["1.0.0.0/5", "8.0.0.0/7", "11.0.0.0/8", "11.0.0.0/8", "12.0.0.0/6", "16.0.0.0/4", "32.0.0.0/3", "64.0.0.0/2", "128.0.0.0/3", "160.0.0.0/5", "168.0.0.0/6", "172.0.0.0/12", "172.32.0.0/11", "172.64.0.0/10", "172.128.0.0/9", "173.0.0.0/8", "174.0.0.0/7", "176.0.0.0/4", "192.0.0.0/9", "192.128.0.0/11", "192.160.0.0/13", "192.169.0.0/16", "192.170.0.0/15", "192.172.0.0/14", "192.176.0.0/12", "192.192.0.0/10", "193.0.0.0/8", "194.0.0.0/7", "196.0.0.0/6", "200.0.0.0/5", "208.0.0.0/4"]
+}
+
+resource "nsxt_ip_set" "PrivateIPs" {
+  description  = "Private IPs provisioned by Terraform"
+  display_name = "Private IP"
+  ip_addresses = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+
 resource "nsxt_ns_group" "Sandbox" {
   display_name = "Sandbox"
   description  = "Sandbox NSGroup provisioned by Terraform"
@@ -67,16 +79,33 @@ resource "nsxt_ns_group" "PrivateCloud" {
   }
 }
 
+
+}
 resource "nsxt_firewall_section" "PrivateCloudGaurdrailWhitelist" {
   description  = "Private Cloud Whitelist Section provisioned by Terraform"
   display_name = "Private Cloud Whitelist"
   section_type = "LAYER3"
   stateful     = true
-   applied_to {
+  applied_to {
     target_type = "NSGroup"
     target_id   = nsxt_ns_group.PrivateCloud.id
   }
-    rule {
+  rule {
+    display_name = "Allow Shared Services"
+    description  = ""
+    action       = "ALLOW"
+    logged       = false
+    ip_protocol  = "IPV4"
+    source {
+      target_type = "NSGroup"
+      target_id   = nsxt_ns_group.PrivateCloud.id
+    }    
+    destination {
+      target_type = "IPSet"
+      target_id   = nsxt_ip_set.SharedServices.id
+    }
+  }
+  rule {
     display_name = "Reject Protected Assets"
     description  = ""
     action       = "REJECT"
@@ -91,8 +120,8 @@ resource "nsxt_firewall_section" "PrivateCloudGaurdrailWhitelist" {
       target_id   = nsxt_ip_set.ProtectedAssets.id
     }
   }
-    rule {
-    display_name = "Allow Sandbox Comms"
+  rule {
+    display_name = "Open inside Sandbox"
     description  = ""
     action       = "ALLOW"
     logged       = false
@@ -107,9 +136,9 @@ resource "nsxt_firewall_section" "PrivateCloudGaurdrailWhitelist" {
     }
   }
     rule {
-    display_name = "Allow Shared Services"
+    display_name = "Private Cloud to Sandbox"
     description  = ""
-    action       = "ALLOW"
+    action       = "Reject"
     logged       = false
     ip_protocol  = "IPV4"
     source {
@@ -117,8 +146,19 @@ resource "nsxt_firewall_section" "PrivateCloudGaurdrailWhitelist" {
       target_id   = nsxt_ns_group.PrivateCloud.id
     }    
     destination {
-      target_type = "IPSet"
-      target_id   = nsxt_ip_set.SharedServices.id
+      target_type = "NSGroup"
+      target_id   = nsxt_ns_group.Sandbox.id
+    }
+  }
+  rule {
+    display_name = "Any to Sandbox"
+    description  = ""
+    action       = "ALLOW"
+    logged       = false
+    ip_protocol  = "IPV4"
+    destination {
+      target_type = "NSGroup"
+      target_id   = nsxt_ns_group.Sandbox.id
     }
   }
 }
@@ -140,17 +180,12 @@ resource "nsxt_firewall_section" "PrivateCloudGaurdrailBlackist" {
     target_type = "NSGroup"
     target_id   = nsxt_ns_group.PrivateCloud.id
   }
-
     rule {
     display_name = "Default Deny (Reject)"
     description  = ""
     action       = "REJECT"
     logged       = false
     ip_protocol  = "IPV4"
-    source {
-      target_type = "NSGroup"
-      target_id   = nsxt_ns_group.PrivateCloud.id
-    }    
     destination {
       target_type = "NSGroup"
       target_id   = nsxt_ns_group.PrivateCloud.id
