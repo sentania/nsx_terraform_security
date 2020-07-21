408772c0f04e92d2bb06d379ed1dadfbba352f15
rpletka <rpletka@vmware.com>
2020-07-20 22:34:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,163 +6,130 @@ provider "nsxt" {
     allow_unverified_ssl = true
 }
 
-resource "nsxt_ip_set" "SharedServices" {
-  description  = "Infrastructure provisioned by Terraform"
-  display_name = "Shared Services"
-  ip_addresses = ["10.100.100.30", "10.101.100.30", "10.100.0.136"]
-}
-
-resource "nsxt_ip_set" "ProtectedAssets" {
-  description  = "Protected Assets provisioned by Terraform"
-  display_name = "Protected Assets"
-  ip_addresses = ["10.100.65.0/24"]
-}
-
-resource "nsxt_ip_set" "Internet" {
-  description  = "Internet IPs provisioned by Terraform"
-  display_name = "Internet IPs"
-  ip_addresses = ["1.0.0.0/8","3.0.0.0/8","4.0.0.0/6", "8.0.0.0/7", "11.0.0.0/8", "11.0.0.0/8", "12.0.0.0/6", "16.0.0.0/4", "32.0.0.0/3", "64.0.0.0/2", "128.0.0.0/3", "160.0.0.0/5", "168.0.0.0/6", "172.0.0.0/12", "172.32.0.0/11", "172.64.0.0/10", "172.128.0.0/9", "173.0.0.0/8", "174.0.0.0/7", "176.0.0.0/4", "192.0.0.0/9", "192.128.0.0/11", "192.160.0.0/13", "192.169.0.0/16", "192.170.0.0/15", "192.172.0.0/14", "192.176.0.0/12", "192.192.0.0/10", "193.0.0.0/8", "194.0.0.0/7", "196.0.0.0/6", "200.0.0.0/5", "208.0.0.0/4"]
-}
-
-resource "nsxt_ip_set" "PrivateIPs" {
-  description  = "Private IPs provisioned by Terraform"
-  display_name = "Private IP"
-  ip_addresses = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
-}
-
-resource "nsxt_ns_group" "Sandbox" {
+resource "nsxt_policy_group" "Sandbox" {
   display_name = "Sandbox"
-  description  = "Sandbox NSGroup provisioned by Terraform"
+  description  = "Sandbox Group provisioned by Terraform"
 }
 
-resource "nsxt_ns_group" "TestPCI" {
+resource "nsxt_policy_group" "TestPCI" {
   display_name = "Test PCI"
-  description  = "Test PCI NSGroup provisioned by Terraform"
+  description  = "Test PCI Group provisioned by Terraform"
 
 }
-resource "nsxt_ns_group" "TestNonPCI" {
+resource "nsxt_policy_group" "TestNonPCI" {
   display_name = "Test Non-PCI"
-  description  = "Test Non-PCI NSGroup provisioned by Terraform"
+  description  = "Test Non-PCI Group provisioned by Terraform"
 
 }
-resource "nsxt_ns_group" "ProdPCI" {
+resource "nsxt_policy_group" "ProdPCI" {
   display_name = "Prod PCI"
-  description  = "Prod PCI NSGroup provisioned by Terraform"
+  description  = "Prod PCI Group provisioned by Terraform"
 
 }
-resource "nsxt_ns_group" "ProdNonPCI" {
+resource "nsxt_policy_group" "ProdNonPCI" {
   display_name = "Prod Non-PCI"
   description  = "Prod Non-PCI NSGroup provisioned by Terraform"
 }
 
-resource "nsxt_ns_group" "PrivateCloud" {
+resource "nsxt_policy_group" "PrivateCloud" {
   display_name = "Private Cloud"
-  description  = "Private Cloud NSGroup provisioned by Terraform"
-  member {
-    target_type = "NSGroup"
-    value = "${nsxt_ns_group.Sandbox.id}"
-  }
-    member {
-    target_type = "NSGroup"
-    value = "${nsxt_ns_group.TestPCI.id}"
-  }
-    member {
-    target_type = "NSGroup"
-    value = "${nsxt_ns_group.TestNonPCI.id}"
-  }
-    member {
-    target_type = "NSGroup"
-    value = "${nsxt_ns_group.ProdPCI.id}"
-  }
-    member {
-    target_type = "NSGroup"
-    value = "${nsxt_ns_group.ProdNonPCI.id}"
+  description  = "Private Cloud Group provisioned by Terraform"
+  criteria {
+    path_expression {
+      member_paths = [nsxt_policy_group.Sandbox.path,nsxt_policy_group.ProdPCI.path,nsxt_policy_group.ProdNonPCI.path, nsxt_policy_group.TestPCI.path, nsxt_policy_group.TestNonPCI.path ]
+    }
   }
 }
 
+resource "nsxt_policy_group" "SharedServices" {
+  display_name = "Shared Services"
+  description  = "Shared Services Group provisioned by Terraform"
+  criteria {
+    ipaddress_expression {
+      ip_addresses = ["10.100.100.30", "10.101.100.30", "10.100.0.136"]
+    }
+  }
+}
 
-resource "nsxt_firewall_section" "PrivateCloudGaurdrailWhitelist" {
+resource "nsxt_policy_group" "ProtectedAssets" {
+  display_name = "Shared Services"
+  description  = "Protected Assets Group provisioned by Terraform"
+  criteria {
+    ipaddress_expression {
+      ip_addresses = ["10.100.65.0/24"]
+    }
+  }
+}
+
+resource "nsxt_policy_group" "InternetCIDR" {
+  display_name = "Internet"
+  description  = "Internet Group provisioned by Terraform"
+  criteria {
+    ipaddress_expression {
+      ip_addresses = ["1.0.0.0/8","3.0.0.0/8","4.0.0.0/6", "8.0.0.0/7", "11.0.0.0/8", "11.0.0.0/8", "12.0.0.0/6", "16.0.0.0/4", "32.0.0.0/3", "64.0.0.0/2", "128.0.0.0/3", "160.0.0.0/5", "168.0.0.0/6", "172.0.0.0/12", "172.32.0.0/11", "172.64.0.0/10", "172.128.0.0/9", "173.0.0.0/8", "174.0.0.0/7", "176.0.0.0/4", "192.0.0.0/9", "192.128.0.0/11", "192.160.0.0/13", "192.169.0.0/16", "192.170.0.0/15", "192.172.0.0/14", "192.176.0.0/12", "192.192.0.0/10", "193.0.0.0/8", "194.0.0.0/7", "196.0.0.0/6", "200.0.0.0/5", "208.0.0.0/4"]
+    }
+  }
+}
+
+resource "nsxt_policy_group" "PrivateIPs" {
+  display_name = "Private IPs"
+  description  = "Private IPs Group provisioned by Terraform"
+  criteria {
+    ipaddress_expression {
+      ip_addresses = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    }
+  }
+}
+
+resource "nsxt_policy_security_policy" "PrivateCloudGaurdrailWhitelist" {
   description  = "Private Cloud Whitelist Section provisioned by Terraform"
   display_name = "Private Cloud Whitelist"
-  section_type = "LAYER3"
-  stateful     = true
-  applied_to {
-    target_type = "NSGroup"
-    target_id   = nsxt_ns_group.PrivateCloud.id
-  }
+  category = "Environment"
   rule {
     display_name = "Allow Shared Services"
     description  = ""
     action       = "ALLOW"
-    logged       = false
-    ip_protocol  = "IPV4"
-    source {
-      target_type = "NSGroup"
-      target_id   = nsxt_ns_group.PrivateCloud.id
-    }    
-    destination {
-      target_type = "IPSet"
-      target_id   = nsxt_ip_set.SharedServices.id
-    }
+    ip_version  = "IPV4"
+    source_groups = [nsxt_policy_group.PrivateCloud.path]
+    destination_groups = [nsxt_policy_group.SharedServices.path]
+    scope = [nsxt_policy_group.PrivateCloud.path, nsxt_policy_group.SharedServices.path]
   }
-  rule {
+   rule {
     display_name = "Reject Protected Assets"
     description  = ""
     action       = "REJECT"
-    logged       = false
-    ip_protocol  = "IPV4"
-    source {
-      target_type = "NSGroup"
-      target_id   = nsxt_ns_group.PrivateCloud.id
-    }    
-    destination {
-      target_type = "IPSet"
-      target_id   = nsxt_ip_set.ProtectedAssets.id
-    }
+    ip_version  = "IPV4"
+    source_groups = [nsxt_policy_group.PrivateCloud.path]
+    destination_groups = [nsxt_policy_group.ProtectedAssets.path]
+    scope = [nsxt_policy_group.PrivateCloud.path, nsxt_policy_group.ProtectedAssets.path]
   }
   rule {
     display_name = "Open inside Sandbox"
     description  = ""
     action       = "ALLOW"
-    logged       = false
-    ip_protocol  = "IPV4"
-    source {
-      target_type = "NSGroup"
-      target_id   = nsxt_ns_group.Sandbox.id
-    }    
-    destination {
-      target_type = "NSGroup"
-      target_id   = nsxt_ns_group.Sandbox.id
-    }
+    ip_version  = "IPV4"
+    source_groups = [nsxt_policy_group.Sandbox.path]
+    destination_groups = [nsxt_policy_group.Sandbox.path]
+    scope = [nsxt_policy_group.Sandbox.path]
   }
     rule {
     display_name = "Private Cloud to Sandbox"
     description  = ""
     action       = "REJECT"
-    logged       = false
-    ip_protocol  = "IPV4"
-    source {
-      target_type = "NSGroup"
-      target_id   = nsxt_ns_group.PrivateCloud.id
-    }    
-    destination {
-      target_type = "NSGroup"
-      target_id   = nsxt_ns_group.Sandbox.id
-    }
+    ip_version  = "IPV4"
+    source_groups = [nsxt_policy_group.PrivateCloud.path]
+    destination_groups = [nsxt_policy_group.Sandbox.path]
+    scope = [nsxt_policy_group.PrivateCloud.path, nsxt_policy_group.Sandbox.path]
   }
   rule {
     display_name = "Any to Sandbox"
     description  = ""
     action       = "ALLOW"
-    logged       = false
-    ip_protocol  = "IPV4"
-    destination {
-      target_type = "NSGroup"
-      target_id   = nsxt_ns_group.Sandbox.id
-    }
+    ip_version  = "IPV4"
+    destination_groups = [nsxt_policy_group.Sandbox.path]
+    scope = [nsxt_policy_group.Sandbox.path]
   }
 }
-
 
 resource "nsxt_firewall_section" "defaultlayer3sect" {
   section_type = "LAYER3"
