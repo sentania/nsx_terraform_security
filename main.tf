1d1af0956cdf985f11e9826b41bb878ab5ee2c90
Scott R. Bowe <scottb@sentania.net>
2020-10-04 21:59:25 -0500

    update

--- main.tf
+++ main.tf
@@ -40,3 +40,87 @@ resource "nsxt_policy_group" "PrivateCloud" {
     }
   }
 }
+
+resource "nsxt_policy_group" "SharedServices" {
+  display_name = "Shared Services"
+  description  = "Shared Services Group provisioned by Terraform"
+  criteria {
+    ipaddress_expression {
+      ip_addresses = ["10.100.0.142","10.100.0.5"]
+    }
+  }
+}
+
+resource "nsxt_policy_group" "ProtectedAssets" {
+  display_name = "Protected Assets"
+  description  = "Protected Assets Group provisioned by Terraform"
+  criteria {
+    ipaddress_expression {
+      ip_addresses = ["10.100.65.0/24"]
+    }
+  }
+}
+
+resource "nsxt_policy_security_policy" "PrivateCloudGaurdrail" {
+  description  = "Private Cloud Guardrails Section provisioned by Terraform"
+  display_name = "Private Cloud Guardrails"
+  category = "Environment"
+  rule {
+    display_name = "Allow Shared Services"
+    description  = ""
+    action       = "ALLOW"
+    ip_version  = "IPV4"
+    source_groups = [nsxt_policy_group.PrivateCloud.path]
+    destination_groups = [nsxt_policy_group.SharedServices.path]
+    scope = [nsxt_policy_group.PrivateCloud.path, nsxt_policy_group.SharedServices.path]
+  }
+   rule {
+    display_name = "Reject Protected Assets"
+    description  = ""
+    action       = "REJECT"
+    ip_version  = "IPV4"
+    source_groups = [nsxt_policy_group.PrivateCloud.path]
+    destination_groups = [nsxt_policy_group.ProtectedAssets.path]
+    scope = [nsxt_policy_group.PrivateCloud.path, nsxt_policy_group.ProtectedAssets.path]
+  }
+  rule {
+    display_name = "Open inside Sandbox"
+    description  = ""
+    action       = "ALLOW"
+    ip_version  = "IPV4"
+    source_groups = [nsxt_policy_group.Sandbox.path]
+    destination_groups = [nsxt_policy_group.Sandbox.path]
+    scope = [nsxt_policy_group.Sandbox.path]
+  }
+    rule {
+    display_name = "Private Cloud to Sandbox"
+    description  = ""
+    action       = "REJECT"
+    ip_version  = "IPV4"
+    source_groups = [nsxt_policy_group.PrivateCloud.path]
+    destination_groups = [nsxt_policy_group.Sandbox.path]
+    scope = [nsxt_policy_group.PrivateCloud.path, nsxt_policy_group.Sandbox.path]
+  }
+  rule {
+    display_name = "Any to Sandbox"
+    description  = ""
+    action       = "ALLOW"
+    ip_version  = "IPV4"
+    destination_groups = [nsxt_policy_group.Sandbox.path]
+    scope = [nsxt_policy_group.Sandbox.path]
+  }
+}
+
+resource "nsxt_policy_security_policy" "PrivateCloudGaurdrailDenyList" {
+  description  = "Private Cloud Default Section provisioned by Terraform"
+  display_name = "Private Cloud Default Deny"
+  category = "Application"
+  sequence_number = 59999
+  rule {
+    display_name = "Default Deny (Reject)"
+    description  = ""
+    action       = "REJECT"
+    ip_version  = "IPV4"
+    destination_groups =  [nsxt_policy_group.PrivateCloud.path]
+  }
+}
