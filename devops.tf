1911975d60abc3d6180b501e8ce315ed828d8c45
Scott R. Bowe <scott@sentania.net>
2020-12-04 13:11:20 -0600

    merge to a single project

---
+++ devops.tf
@@ -0,0 +1,196 @@
+resource "nsxt_policy_group" "AllowSSH" {
+  display_name = "Allow SSH"
+  description  = "Allow SSH Group provisioned by Terraform"
+  criteria {
+      condition {
+          key         = "Tag"
+          member_type = "SegmentPort"
+          operator    = "EQUALS"
+          value       = "AllowSSH|Role"
+      }
+    }
+    tag {
+        scope = "AllowSSH"
+        tag   = "Role"
+    }
+}
+
+resource "nsxt_policy_group" "MySQLClients" {
+  display_name = "MySQLClients"
+  description  = "MySQLClients Group provisioned by Terraform"
+  criteria {
+      condition {
+          key         = "Tag"
+          member_type = "SegmentPort"
+          operator    = "EQUALS"
+          value       = "MySQLClient|Role"
+      }
+    }
+    tag {
+        scope = "MySQLClient"
+        tag   = "Role"
+    }
+}
+
+resource "nsxt_policy_group" "MySQLServers" {
+  display_name = "MySQLServers"
+  description  = "MySQLServers Group provisioned by Terraform"
+    criteria {
+        condition {
+            key         = "Tag"
+            member_type = "SegmentPort"
+            operator    = "EQUALS"
+            value       = "MySQLServer|Role"
+        }
+    }
+    tag {
+    scope = "MySQLServer"
+    tag   = "Role"
+  }
+}
+
+resource "nsxt_policy_group" "WebServers" {
+    display_name = "WebServers"
+    description  = "WebServers Group provisioned by Terraform"
+    criteria {
+        condition {
+            key         = "Tag"
+            member_type = "SegmentPort"
+            operator    = "EQUALS"
+            value       = "WebServer|Role"
+        }
+    }
+    tag {
+        scope = "WebServer"
+        tag   = "Role"
+    }
+}
+
+resource "nsxt_policy_group" "MSSQLServers" {
+    display_name = "MSSQL Servers"
+    description  = "MSSQL Servers Group provisioned by Terraform"
+    criteria {
+        condition {
+            key         = "Tag"
+            member_type = "SegmentPort"
+            operator    = "EQUALS"
+            value       = "MSSQLServer|Role"
+        }
+    }
+    tag {
+        scope = "MSSQLServer"
+        tag   = "Role"
+    }
+}
+
+resource "nsxt_policy_group" "MSSQLClients" {
+    display_name = "MSSQL Clients"
+    description  = "MSSQL Clients Group provisioned by Terraform"
+    criteria {
+        condition {
+            key         = "Tag"
+            member_type = "SegmentPort"
+            operator    = "EQUALS"
+            value       = "MSSQLClient|Role"
+        }
+    }
+    tag {
+        scope = "MSSQLClient"
+        tag   = "Role"
+    }
+}
+
+resource "nsxt_policy_service" "WebServerServices" {
+  description  = "Web Server Serivces provisioned by Terraform"
+  display_name = "Web Server Services"
+
+  l4_port_set_entry {
+    display_name      = "Web Server Services"
+    description       = "TCP port 80 and 443"
+    protocol          = "TCP"
+    destination_ports = [ "80", "443" ]
+  }
+  
+}
+
+resource "nsxt_policy_service" "MySQLServices" {
+  description  = "MySQL Serivces provisioned by Terraform"
+  display_name = "MySQL Services"
+
+  l4_port_set_entry {
+    display_name      = "MySQL Server Services"
+    description       = "TCP port 3306"
+    protocol          = "TCP"
+    destination_ports = [ "3306" ]
+  }
+}
+
+resource "nsxt_policy_service" "MSSQLServices" {
+  description  = "MSSQL Serivces provisioned by Terraform"
+  display_name = "MSSQL Services"
+
+  l4_port_set_entry {
+    display_name      = "MSSQL Server Services"
+    description       = "TCP port 1433"
+    protocol          = "TCP"
+    destination_ports = [ "1433" ]
+  }
+}
+
+resource "nsxt_policy_service" "SSHService" {
+  description  = "SSH Serivces provisioned by Terraform"
+  display_name = "SSH Services"
+
+  l4_port_set_entry {
+    display_name      = "SSH Server Services"
+    description       = "TCP port 22"
+    protocol          = "TCP"
+    destination_ports = [ "22" ]
+  }
+}
+
+resource "nsxt_policy_security_policy" "PrivateCloudPolicies" {
+  description  = "Private Cloud Blueprint Policies Section provisioned by Terraform"
+  display_name = "Private Cloud Blueprint Policies"
+  category = "Application"
+  rule {
+    display_name = "Web Traffic"
+    description  = ""
+    action       = "ALLOW"
+    ip_version  = "IPV4"
+    services = [nsxt_policy_service.WebServerServices.path]
+    destination_groups = [nsxt_policy_group.WebServers.path]
+    scope = [nsxt_policy_group.WebServers.path]
+  }
+    rule {
+    display_name = "MySQL Traffic"
+    description  = ""
+    action       = "ALLOW"
+    ip_version  = "IPV4"
+    services = [nsxt_policy_service.MySQLServices.path]
+    source_groups = [nsxt_policy_group.MySQLClients.path]
+    destination_groups = [nsxt_policy_group.MySQLServers.path]
+    scope = [nsxt_policy_group.MySQLClients.path,nsxt_policy_group.MySQLServers.path]
+  }
+
+    rule {
+    display_name = "MySQL Traffic"
+    description  = ""
+    action       = "ALLOW"
+    ip_version  = "IPV4"
+    services = [nsxt_policy_service.MSSQLServices.path]
+    source_groups = [nsxt_policy_group.MSSQLClients.path]
+    destination_groups = [nsxt_policy_group.MSSQLServers.path]
+    scope = [nsxt_policy_group.MySQLClients.path,nsxt_policy_group.MySQLServers.path]
+  }
+
+    rule {
+    display_name = "SSH Traffic"
+    description  = ""
+    action       = "ALLOW"
+    ip_version  = "IPV4"
+    services = [nsxt_policy_service.SSHService.path]
+    destination_groups = [nsxt_policy_group.AllowSSH.path]
+    scope = [nsxt_policy_group.AllowSSH.path]
+  }
+}
---
+++ privateCloudGuardrails.tf
@@ -0,0 +1,179 @@
+resource "nsxt_policy_group" "Sandbox" {
+  display_name = "Sandbox"
+  description  = "Sandbox Group provisioned by Terraform"
+  criteria {
+      condition {
+          key         = "Tag"
+          member_type = "SegmentPort"
+          operator    = "EQUALS"
+          value       = "sandbox|environment"
+      }
+    }
+}
+
+resource "nsxt_policy_group" "TestPCI" {
+  display_name = "Test PCI"
+  description  = "Test PCI Group provisioned by Terraform"
+  criteria {
+      condition {
+          key         = "Tag"
+          member_type = "SegmentPort"
+          operator    = "EQUALS"
+          value       = "test|environment"
+      }
+      condition {
+          key         = "Tag"
+          member_type = "SegmentPort"
+          operator    = "EQUALS"
+          value       = "pci|compliance"
+      }
+    }
+}
+resource "nsxt_policy_group" "TestNonPCI" {
+  display_name = "Test Non-PCI"
+  description  = "Test Non-PCI Group provisioned by Terraform"
+    criteria {
+      condition {
+          key         = "Tag"
+          member_type = "SegmentPort"
+          operator    = "EQUALS"
+          value       = "test|environment"
+      }
+      condition {
+          key         = "Tag"
+          member_type = "SegmentPort"
+          operator    = "EQUALS"
+          value       = "nonpci|compliance"
+      }
+    }
+}
+resource "nsxt_policy_group" "ProdPCI" {
+  display_name = "Prod PCI"
+  description  = "Prod PCI Group provisioned by Terraform"
+  criteria {
+      condition {
+          key         = "Tag"
+          member_type = "SegmentPort"
+          operator    = "EQUALS"
+          value       = "prod|environment"
+      }
+      condition {
+          key         = "Tag"
+          member_type = "SegmentPort"
+          operator    = "EQUALS"
+          value       = "pci|compliance"
+      }
+    }
+}
+resource "nsxt_policy_group" "ProdNonPCI" {
+  display_name = "Prod Non-PCI"
+  description  = "Prod Non-PCI NSGroup provisioned by Terraform"
+    criteria {
+      condition {
+          key         = "Tag"
+          member_type = "SegmentPort"
+          operator    = "EQUALS"
+          value       = "prod|environment"
+      }
+      condition {
+          key         = "Tag"
+          member_type = "SegmentPort"
+          operator    = "EQUALS"
+          value       = "nonpci|compliance"
+      }
+    }
+}
+
+resource "nsxt_policy_group" "PrivateCloud" {
+  display_name = "Private Cloud 2.0"
+  description  = "Private Cloud Group provisioned by Terraform"
+  criteria {
+    path_expression {
+      member_paths = [nsxt_policy_group.Sandbox.path,nsxt_policy_group.ProdPCI.path,nsxt_policy_group.ProdNonPCI.path, nsxt_policy_group.TestPCI.path, nsxt_policy_group.TestNonPCI.path ]
+    }
+  }
+}
+
+resource "nsxt_policy_group" "SharedServices" {
+  display_name = "Shared Services"
+  description  = "Shared Services Group provisioned by Terraform"
+  criteria {
+    ipaddress_expression {
+      ip_addresses = ["10.100.0.142","10.100.0.5"]
+    }
+  }
+}
+
+resource "nsxt_policy_group" "ProtectedAssets" {
+  display_name = "Protected Assets"
+  description  = "Protected Assets Group provisioned by Terraform"
+  criteria {
+    ipaddress_expression {
+      ip_addresses = ["10.100.65.0/24"]
+    }
+  }
+}
+
+resource "nsxt_policy_security_policy" "PrivateCloudGuardrail" {
+  description  = "Private Cloud Guardrails Section provisioned by Terraform"
+  display_name = "Private Cloud Guardrails"
+  category = "Environment"
+  rule {
+    display_name = "Allow Shared Services"
+    description  = ""
+    action       = "ALLOW"
+    ip_version  = "IPV4"
+    source_groups = [nsxt_policy_group.PrivateCloud.path]
+    destination_groups = [nsxt_policy_group.SharedServices.path]
+    scope = [nsxt_policy_group.PrivateCloud.path, nsxt_policy_group.SharedServices.path]
+  }
+   rule {
+    display_name = "Reject Protected Assets"
+    description  = ""
+    action       = "REJECT"
+    ip_version  = "IPV4"
+    source_groups = [nsxt_policy_group.PrivateCloud.path]
+    destination_groups = [nsxt_policy_group.ProtectedAssets.path]
+    scope = [nsxt_policy_group.PrivateCloud.path, nsxt_policy_group.ProtectedAssets.path]
+  }
+  rule {
+    display_name = "Open inside Sandbox"
+    description  = ""
+    action       = "ALLOW"
+    ip_version  = "IPV4"
+    source_groups = [nsxt_policy_group.Sandbox.path]
+    destination_groups = [nsxt_policy_group.Sandbox.path]
+    scope = [nsxt_policy_group.Sandbox.path]
+  }
+    rule {
+    display_name = "Private Cloud to Sandbox"
+    description  = ""
+    action       = "REJECT"
+    ip_version  = "IPV4"
+    source_groups = [nsxt_policy_group.PrivateCloud.path]
+    destination_groups = [nsxt_policy_group.Sandbox.path]
+    scope = [nsxt_policy_group.PrivateCloud.path, nsxt_policy_group.Sandbox.path]
+  }
+  rule {
+    display_name = "Any to Sandbox"
+    description  = ""
+    action       = "ALLOW"
+    ip_version  = "IPV4"
+    destination_groups = [nsxt_policy_group.Sandbox.path]
+    scope = [nsxt_policy_group.Sandbox.path]
+  }
+}
+
+resource "nsxt_policy_security_policy" "PrivateCloudGuardrailDenyList" {
+  description  = "Private Cloud Default Section provisioned by Terraform"
+  display_name = "Private Cloud Default Deny"
+  category = "Application"
+  sequence_number = 59999
+  rule {
+    display_name = "Default Deny (Reject)"
+    description  = ""
+    action       = "REJECT"
+    ip_version  = "IPV4"
+    destination_groups =  [nsxt_policy_group.PrivateCloud.path]
+  }
+}
