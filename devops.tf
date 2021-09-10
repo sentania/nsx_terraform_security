37a83f1515c97e4bd346d89e2a4263ddd3556fc8
Scott R. Bowe <scott@sentania.net>
2021-09-10 09:01:13 -0500

    clean up code

--- devops.tf
+++ devops.tf
@@ -184,23 +184,7 @@ resource "nsxt_policy_security_policy" "PrivateCloudPolicies" {
   description  = "Private Cloud Devops Policies Section provisioned by Terraform"
   display_name = "Private Cloud Devops Policies"
   category = "Application"
-  lifecycle {
-    create_before_destroy = true
-  }
-  depends_on = [                                   ####The NSX T provider/TF interaction does not properly create a depenency map - so we need to define explicit dependencies to aid on
-    nsxt_policy_service.SSHService,
-    nsxt_policy_service.MySQLServices,
-    nsxt_policy_service.MSSQLServices,
-    nsxt_policy_service.WebServerServices,
-    nsxt_policy_service.RDPService,
-    nsxt_policy_group.AllowRDP,
-    nsxt_policy_group.AllowSSH,
-    nsxt_policy_group.MySQLClients,
-    nsxt_policy_group.MySQLServers,
-    nsxt_policy_group.WebServers,
-    nsxt_policy_group.MSSQLServers,
-    nsxt_policy_group.MSSQLClients,
-  ]
+
   rule {
     display_name = "Web Traffic"
     description  = ""
